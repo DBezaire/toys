e137cb2d5eed67805b252996af1534c9fed7cf71
Dave Bezaire <dlbezaire@gmail.com>
2021-07-17 19:32:49 -0400

    added /times to express.js

--- express.js
+++ express.js
@@ -2,7 +2,17 @@ const express = require('express');
 const app = express();
 const port = 7000;
 
-app.get('/', (req, res) => res.send("got this served"));
+app
+  .get('/', (req, res) => res.send("got this served"))
+  .get('/times', (req, res) => res.send(showTimes()))
+  .listen(port, () => console.log(`Express listening on port: ${port}`))
+;
 
-app.listen(port, () => console.log(`Express listening on port: ${port}`));
-
+showTimes = () => {
+  let result  = '';
+  const times = process.env.TIMES || 5;
+  for (i = 0; i < times; i++) {
+    result += i + ' ';
+  }
+  return result;
+}
