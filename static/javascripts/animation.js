48a480116df3d715a76772436d0580f2912ab4e3
Dave Bezaire <dlbezaire@gmail.com>
2021-08-01 12:24:56 -0400

    added structure to test various animations

--- static/javascripts/animation.js
+++ static/javascripts/animation.js
@@ -9,7 +9,7 @@ function setupTestAnimation(button) {
     setupAnimations();
     // assume button's parent contains checkboxes and controls
     let parent = button.parentElement;
-    makeCheckboxes(parent); 
+    makeCheckboxes(parent);
     parent.querySelector('ul.controls').style.display = 'block';
     button.innerHTML = 'play';
   } else {
@@ -24,13 +24,13 @@ function setupAnimations() {
 //  let audioContext = new AudioContext();
 
   for (let a of Object.getOwnPropertyNames(animationClasses)) {
-    animations[a] = new animationClasses[a]();
+    animations[a] = new animationClasses[a](document.getElementById('cob'));
   }
   console.log('animations set up complete');
 }
 
 function makeCheckboxes(div) {
-// add a UL to div with a checkbox for each 
+// add a UL to div with a checkbox for each
 // sound the global sounds object
 // and store the checkboxes in a global checkboxes object
   let ul = document.createElement('UL');
@@ -46,21 +46,23 @@ function makeCheckboxes(div) {
     li.appendChild(label);
     li.appendChild(input);
     ul.appendChild(li);
-    checkboxes[s] = input;
+    checkboxes[a] = input;
   }
   div.appendChild(ul);
   console.log('checkboxes set up complete');
 }
 
 function playAnimations() {
+  animations.CircleOverBox.trigger();
+  return;
 // play checked animations
   let playingTimeSeconds = Number(document.querySelector("input#play-time").value);
   let beatLengthSeconds = Math.min(playingTimeSeconds, Number(document.querySelector("input#beat-length").value));
-  let volume = Number(document.querySelector("input#volume").value);
-  
+  let speed = Number(document.querySelector("input#speed").value);
+
   let beatsPerMeasure = 4;
-  
-  // count number checked and calculate 
+
+  // count number checked and calculate
   // duration of each sound within a beat
   let numChecked = 0;
   for (let key in checkboxes) {
@@ -69,19 +71,19 @@ function playAnimations() {
   let duration = beatLengthSeconds/numChecked;
 
   // schedule the sounds within each beat to fill the playing time
-  let nowTime = Math.ceil(sounds.Sound.context.currentTime);
+  let nowTime = new Date(); // Math.ceil(sounds.Sound.context.currentTime);
   let beatCount = 0;
   for (let beatTime = 0; beatTime < playingTimeSeconds; beatTime += beatLengthSeconds) {
     beatCount++;
     let beatInMeasure = beatCount % beatsPerMeasure;
     let sequenceInBeat = 0;
-    for (let key in sounds) {
+    for (let key in animations) {
       if(checkboxes[key].checked) {
-        sounds[key].trigger(nowTime + beatTime + (sequenceInBeat * duration), beatInMeasure, volume, duration);
+        animations[key].trigger(nowTime + beatTime + (sequenceInBeat * duration), beatInMeasure, speed, duration);
         sequenceInBeat++;
       }
     }
-  } 
+  }
 }
 
 animationClasses.Base = class {
@@ -89,41 +91,41 @@ animationClasses.Base = class {
 // Also sets initial attack, decay, sustain, and release that usually get replaced by extensions.
 // Extensions always replace shapeSound() and set equalizeFactor to balance against each other.
 
-  constructor() {
+  constructor(container) {
+    this.container = container;
     this.animationName = 'base class';
     this.duration = .05;
-    
+
     // this.speed is set externally by the user in real time from 0 to 100 to adjust viewing level.
     // The value set here is just a starting point.
     // The speed is applied to all sounds from all subclasses.
     this.speed = 25;
-    
+
     // this.equalizeFactor is set individually in each animation subclass to make them play roughly the same.
     // It is a multiplier, so the default value of 1 set here has no effect.
     this.equalizeFactor = 1;
-    
+
     this.quietTimeAtStartSeconds = 0; // seconds of quiet before starting sound
     this.attackTime = .002; // seconds from zero to full volume
     this.decayTime = .002; // seconds from full volume to sustain volume
     this.sustainLevel = 1; // proportion of full volume held
     this.releaseTime = .002; // seconds from sustain volume to zero
     this.quietTimeAtEndProportion = .2; // proportion of duration silent at end
-
   }
-  
+
   trigger(time, beatInMeasure = 1, speed = this.speed, duration = this.duration) {
-    if (typeof time == 'undefined') time = this.context.currentTime;
-    this.calcSpeed = this.calculateSpeed(volume);
+    // if (typeof time == 'undefined') time = this.context.currentTime;
+    this.calcSpeed = this.calculateSpeed(speed);
     this.calcDuration = duration;
     this.shapeAnimation(time, beatInMeasure);
   }
-  
+
   shapeAnimation() {
-    let item = ()
-    setSettings(item, 0);
-    alert('no shapeSound() defined');
+    let item = {};
+    this.setSettings(item, 0);
+    alert('no shapeAnimation() defined');
   }
-  
+
   calculateSpeed(speed) {
     return speed;
   }
@@ -137,21 +139,21 @@ animationClasses.Base = class {
     let sustainLevel = paramOverride('sustainLevel');
     let releaseTime = paramOverride('releaseTime');
     let quietTimeAtEndProportion = paramOverride('quietTimeAtEndProportion');
-    
+
     this.maxGain = (params !== null && params.hasOwnProperty('maxGain')) ? params['maxGain'] : this.calcGain;
 
     this.endBeat = time + this.calcDuration;
     this.endSound =  this.endBeat - (this.calcDuration * quietTimeAtEndProportion);
     this.endSustain = this.endSound - releaseTime;
-    
+
     this.endSilence = time + quietTimeAtStartSeconds;
     this.endAttack = this.endSilence + attackTime;
     this.endDecay = Math.min(this.endSustain, this.endAttack + decayTime);
-    
+
     this.sustainGain = this.maxGain * sustainLevel;
-    
+
     item.someSetting = 'some setting';
-    
+
     console.log(` time:         ${time} - ${this.soundName}
                   endSilence:   ${this.endSilence}
                   endAttack:    ${this.endAttack}
@@ -168,14 +170,38 @@ animationClasses.Base = class {
 }
 
 animationClasses.CircleOverBox = class extends animationClasses.Base {
-  constructor(context) {
-    super(context);
+  constructor(container) {
+    super(container);
     this.animationName = 'CircleOverBox';
     this.equalizeFactor = .6;
+
+    this.box = this.container.querySelector('.box');
+    this.circle = this.container.querySelector('.circle');
   }
-  
+
   shapeAnimation(time, beatInMeasure) {
-    alert(`this would be the ${this.animationName} process`);
+    let boxX1 = this.box.offsetLeft;
+    let boxY1 = this.box.offsetTop;
+    let boxW = this.box.offsetWidth;
+    let boxH = this.box.offsetHeight;
+    let boxX2 = boxX1 + boxW;
+    let boxY2 = boxY1 + boxH;
+    let circleX1 = this.circle.offsetLeft;
+    let circleY1 = this.circle.offsetTop;
+    let circleW = this.circle.offsetWidth;
+    let circleH = this.circle.offsetHeight;
+    let circleX2 = circleX1 + circleW;
+    let circleY2 = circleY1 + circleH;
+
+    let circleXMin = boxX1;
+    let circleYMin = boxY1;
+    
+    let circleXMax = boxX2 - circleW;
+    let circleYMax = boxY2 - circleH;
+
+    this.circle.style.left = circleXMax + 'px';
+    this.circle.style.top = circleYMax + 'px';
+    // alert(`this would be the ${this.animationName} process`);
   }
 }
 
